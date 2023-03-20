18fbeaec66b6fb6cabdf56d66875c33239308f96
coraline9d <120565448+coraline9d@users.noreply.github.com>
2023-03-20 18:25:12 +0100

    Security is ok

--- dump_theater.sql
+++ dump_theater.sql
@@ -23,11 +23,11 @@ DROP TABLE IF EXISTS `admins`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `admins` (
-  `id_admins` int(11) NOT NULL AUTO_INCREMENT,
+  `id_admins` char(36) NOT NULL,
   `email` varchar(50) NOT NULL,
   `admins_password` varchar(60) NOT NULL,
   PRIMARY KEY (`id_admins`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -36,7 +36,7 @@ CREATE TABLE `admins` (
 
 LOCK TABLES `admins` WRITE;
 /*!40000 ALTER TABLE `admins` DISABLE KEYS */;
-INSERT INTO `admins` VALUES (1,'choyer0@facebook.com','$2y$10$Uh38.Z6VwzcoATDQvLBrBevYrEXX3aRz/Ta9vAaS/lnMyPj0Et3ri'),(2,'spenhall1@netlog.com','$2y$10$cCXnEYzk4O1K/iK896fCO.lsxS18.Qc2GlYsZK1W68mvpjQXOLp/u'),(3,'fscruton2@about.com','$2y$10$WldmBh0chrIbs7yMItwNneW82jogNd0ULCTKN3k.W5c.nR8JjSKBW'),(4,'kpapierz3@blogs.com','$2y$10$I/FTiBhGwgAtGKLN2wz7sOIOX.lgVi3w3wS.lLNmIZPVE/tvszR1K');
+INSERT INTO `admins` VALUES ('58f9eb0a-c742-11ed-81af-d3284c073c1a','choyer0@facebook.com','$2y$10$Uh38.Z6VwzcoATDQvLBrBevYrEXX3aRz/Ta9vAaS/lnMyPj0Et3ri'),('58f9f97e-c742-11ed-81af-d3284c073c1a','spenhall1@netlog.com','$2y$10$cCXnEYzk4O1K/iK896fCO.lsxS18.Qc2GlYsZK1W68mvpjQXOLp/u'),('58fa03ce-c742-11ed-81af-d3284c073c1a','fscruton2@about.com','$2y$10$WldmBh0chrIbs7yMItwNneW82jogNd0ULCTKN3k.W5c.nR8JjSKBW'),('58fa0e00-c742-11ed-81af-d3284c073c1a','kpapierz3@blogs.com','$2y$10$I/FTiBhGwgAtGKLN2wz7sOIOX.lgVi3w3wS.lLNmIZPVE/tvszR1K');
 /*!40000 ALTER TABLE `admins` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -48,8 +48,8 @@ DROP TABLE IF EXISTS `admins_complexes`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `admins_complexes` (
-  `id_admins` int(11) NOT NULL,
-  `id_complexes` int(11) NOT NULL,
+  `id_admins` char(36) NOT NULL,
+  `id_complexes` char(36) NOT NULL,
   KEY `id_admins` (`id_admins`),
   KEY `id_complexes` (`id_complexes`),
   CONSTRAINT `admins_complexes_ibfk_1` FOREIGN KEY (`id_admins`) REFERENCES `admins` (`id_admins`),
@@ -63,7 +63,6 @@ CREATE TABLE `admins_complexes` (
 
 LOCK TABLES `admins_complexes` WRITE;
 /*!40000 ALTER TABLE `admins_complexes` DISABLE KEYS */;
-INSERT INTO `admins_complexes` VALUES (1,1),(2,2),(3,3),(4,4);
 /*!40000 ALTER TABLE `admins_complexes` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -75,11 +74,11 @@ DROP TABLE IF EXISTS `complexes`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `complexes` (
-  `id_complexes` int(11) NOT NULL AUTO_INCREMENT,
+  `id_complexes` char(36) NOT NULL,
   `name` varchar(50) NOT NULL,
   `address` varchar(50) NOT NULL,
   PRIMARY KEY (`id_complexes`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -88,7 +87,7 @@ CREATE TABLE `complexes` (
 
 LOCK TABLES `complexes` WRITE;
 /*!40000 ALTER TABLE `complexes` DISABLE KEYS */;
-INSERT INTO `complexes` VALUES (1,'Thoughtbridge','24593 Tomscot Road'),(2,'Pixope','801 Scofield Way'),(3,'Dazzlesphere','27792 Maywood Road'),(4,'Quire','68252 Esker Plaza');
+INSERT INTO `complexes` VALUES ('58f9a834-c742-11ed-81af-d3284c073c1a','Thoughtbridge','24593 Tomscot Road'),('58f9b7a2-c742-11ed-81af-d3284c073c1a','Pixope','801 Scofield Way'),('58f9c274-c742-11ed-81af-d3284c073c1a','Dazzlesphere','27792 Maywood Road'),('58f9cf80-c742-11ed-81af-d3284c073c1a','Quire','68252 Esker Plaza');
 /*!40000 ALTER TABLE `complexes` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -100,11 +99,11 @@ DROP TABLE IF EXISTS `movie`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `movie` (
-  `id_movie` int(11) NOT NULL AUTO_INCREMENT,
+  `id_movie` char(36) NOT NULL,
   `title` varchar(50) NOT NULL,
   `synopsis` text NOT NULL,
   PRIMARY KEY (`id_movie`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -113,7 +112,7 @@ CREATE TABLE `movie` (
 
 LOCK TABLES `movie` WRITE;
 /*!40000 ALTER TABLE `movie` DISABLE KEYS */;
-INSERT INTO `movie` VALUES (1,'Good Lawyer\'s Wife, A (Baramnan gajok)','Cysto & recto w grf/pros'),(2,'Tadpole','Bact smear-eye'),(3,'Pericles on 31st Street','Parathyroid biopsy'),(4,'Great Race, The','Wound catheter irrigat'),(5,'Kedma','Close reduc-femur epiphy');
+INSERT INTO `movie` VALUES ('58fa2610-c742-11ed-81af-d3284c073c1a','Good Lawyer\'s Wife, A (Baramnan gajok)','Cysto & recto w grf/pros'),('58fa30d8-c742-11ed-81af-d3284c073c1a','Tadpole','Bact smear-eye'),('58fa3e02-c742-11ed-81af-d3284c073c1a','Pericles on 31st Street','Parathyroid biopsy'),('58fa4b04-c742-11ed-81af-d3284c073c1a','Great Race, The','Wound catheter irrigat'),('58fa566c-c742-11ed-81af-d3284c073c1a','Kedma','Close reduc-femur epiphy');
 /*!40000 ALTER TABLE `movie` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -125,14 +124,14 @@ DROP TABLE IF EXISTS `movie_theater`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `movie_theater` (
-  `id_movie_theater` int(11) NOT NULL AUTO_INCREMENT,
+  `id_movie_theater` char(36) NOT NULL,
   `number_of_the_movie_room` int(11) NOT NULL,
   `number_of_seat` int(11) NOT NULL,
-  `id_movie` int(11) NOT NULL,
+  `id_movie` char(36) NOT NULL,
   PRIMARY KEY (`id_movie_theater`),
   KEY `id_movie` (`id_movie`),
   CONSTRAINT `movie_theater_ibfk_1` FOREIGN KEY (`id_movie`) REFERENCES `movie` (`id_movie`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -141,7 +140,6 @@ CREATE TABLE `movie_theater` (
 
 LOCK TABLES `movie_theater` WRITE;
 /*!40000 ALTER TABLE `movie_theater` DISABLE KEYS */;
-INSERT INTO `movie_theater` VALUES (1,8,100,1),(2,7,200,2),(3,9,230,3),(4,5,140,4),(5,3,250,5);
 /*!40000 ALTER TABLE `movie_theater` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -153,11 +151,11 @@ DROP TABLE IF EXISTS `rate`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `rate` (
-  `id_rate` int(11) NOT NULL AUTO_INCREMENT,
+  `id_rate` char(36) NOT NULL,
   `category` varchar(50) NOT NULL,
   `price` float NOT NULL,
   PRIMARY KEY (`id_rate`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -166,7 +164,7 @@ CREATE TABLE `rate` (
 
 LOCK TABLES `rate` WRITE;
 /*!40000 ALTER TABLE `rate` DISABLE KEYS */;
-INSERT INTO `rate` VALUES (1,'Plein Tarif',9.2),(2,'Étudiant',7.6),(3,'Moins de 14 ans',5.9);
+INSERT INTO `rate` VALUES ('58f918b0-c742-11ed-81af-d3284c073c1a','Plein Tarif',9.2),('58f926ac-c742-11ed-81af-d3284c073c1a','Étudiant',7.6),('58f93174-c742-11ed-81af-d3284c073c1a','Moins de 14 ans',5.9);
 /*!40000 ALTER TABLE `rate` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -178,12 +176,12 @@ DROP TABLE IF EXISTS `reservation`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `reservation` (
-  `id_reservation` int(11) NOT NULL AUTO_INCREMENT,
+  `id_reservation` char(36) NOT NULL,
   `number_of_seat` int(11) NOT NULL,
   `name` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
   PRIMARY KEY (`id_reservation`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -192,7 +190,6 @@ CREATE TABLE `reservation` (
 
 LOCK TABLES `reservation` WRITE;
 /*!40000 ALTER TABLE `reservation` DISABLE KEYS */;
-INSERT INTO `reservation` VALUES (1,1,'Josiah Torn','jtorn0@youtu.be'),(2,2,'Adrian Leishman','aleishman1@hud.gov'),(3,3,'Lyle Beak','lbeak2@comcast.net'),(4,4,'Kiley Cubbin','kcubbin3@blog.com'),(5,5,'Thorndike Hollidge','thollidge4@a8.net');
 /*!40000 ALTER TABLE `reservation` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -204,20 +201,23 @@ DROP TABLE IF EXISTS `session`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `session` (
-  `id_session` int(11) NOT NULL AUTO_INCREMENT,
+  `id_session` char(36) NOT NULL,
   `day` date NOT NULL,
   `schedule` time NOT NULL,
-  `id_users` int(11) NOT NULL,
-  `id_complexes` int(11) NOT NULL,
-  `id_rate` int(11) NOT NULL,
+  `id_users` char(36) NOT NULL,
+  `id_complexes` char(36) NOT NULL,
+  `id_rate` char(36) NOT NULL,
+  `id_reservation` char(36) NOT NULL,
   PRIMARY KEY (`id_session`),
   KEY `id_users` (`id_users`),
   KEY `id_complexes` (`id_complexes`),
   KEY `id_rate` (`id_rate`),
+  KEY `id_reservation` (`id_reservation`),
   CONSTRAINT `session_ibfk_1` FOREIGN KEY (`id_users`) REFERENCES `users` (`id_users`),
   CONSTRAINT `session_ibfk_2` FOREIGN KEY (`id_complexes`) REFERENCES `complexes` (`id_complexes`),
-  CONSTRAINT `session_ibfk_3` FOREIGN KEY (`id_rate`) REFERENCES `rate` (`id_rate`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+  CONSTRAINT `session_ibfk_3` FOREIGN KEY (`id_rate`) REFERENCES `rate` (`id_rate`),
+  CONSTRAINT `session_ibfk_4` FOREIGN KEY (`id_reservation`) REFERENCES `reservation` (`id_reservation`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -226,7 +226,6 @@ CREATE TABLE `session` (
 
 LOCK TABLES `session` WRITE;
 /*!40000 ALTER TABLE `session` DISABLE KEYS */;
-INSERT INTO `session` VALUES (1,'2023-02-27','09:15:00',1,1,1),(2,'2023-02-23','09:20:00',2,2,2),(3,'2023-02-24','09:25:00',3,3,3);
 /*!40000 ALTER TABLE `session` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -238,8 +237,8 @@ DROP TABLE IF EXISTS `session_movie_theater`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `session_movie_theater` (
-  `id_session` int(11) NOT NULL,
-  `id_movie_theater` int(11) NOT NULL,
+  `id_session` char(36) NOT NULL,
+  `id_movie_theater` char(36) NOT NULL,
   KEY `id_session` (`id_session`),
   KEY `id_movie_theater` (`id_movie_theater`),
   CONSTRAINT `session_movie_theater_ibfk_1` FOREIGN KEY (`id_session`) REFERENCES `session` (`id_session`),
@@ -253,35 +252,7 @@ CREATE TABLE `session_movie_theater` (
 
 LOCK TABLES `session_movie_theater` WRITE;
 /*!40000 ALTER TABLE `session_movie_theater` DISABLE KEYS */;
-INSERT INTO `session_movie_theater` VALUES (1,1),(2,2),(3,3);
 /*!40000 ALTER TABLE `session_movie_theater` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `session_reservation`
---
-
-DROP TABLE IF EXISTS `session_reservation`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `session_reservation` (
-  `id_session` int(11) NOT NULL,
-  `id_reservation` int(11) NOT NULL,
-  KEY `id_session` (`id_session`),
-  KEY `id_reservation` (`id_reservation`),
-  CONSTRAINT `session_reservation_ibfk_1` FOREIGN KEY (`id_session`) REFERENCES `session` (`id_session`),
-  CONSTRAINT `session_reservation_ibfk_2` FOREIGN KEY (`id_reservation`) REFERENCES `reservation` (`id_reservation`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `session_reservation`
---
-
-LOCK TABLES `session_reservation` WRITE;
-/*!40000 ALTER TABLE `session_reservation` DISABLE KEYS */;
-INSERT INTO `session_reservation` VALUES (1,1),(2,2),(3,3);
-/*!40000 ALTER TABLE `session_reservation` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -292,11 +263,11 @@ DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
-  `id_users` int(11) NOT NULL AUTO_INCREMENT,
+  `id_users` char(36) NOT NULL,
   `email` varchar(50) NOT NULL,
   `users_password` varchar(60) NOT NULL,
   PRIMARY KEY (`id_users`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -305,7 +276,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'gissard0@harvard.edu','$2y$10$42E.edBFNa5DnF1Jo4PFAekXP/awVNYDwE5.h219m2I7Ud8QrJjkq'),(2,'ncrumb1@wisc.edu','$2y$10$30Lu8NBepzLRwBAbYDX20edv1zuXMgeDrDLcEXxLIqaNDbu.Tf8Iu'),(3,'cromanin2@nature.com','$2y$10$bY.iyLaTtuMQTZOxkL9tEuLX8UCiUssQpSZXYLafEXUwqXgoWYzyO'),(4,'fcumberpatch3@themeforest.net','$2y$10$dqkdCUuDxsdoMFOA3VoPKO6J8ekGeUFMo3r3/49mA0rO3jDGFWIsW');
+INSERT INTO `users` VALUES ('58f95b90-c742-11ed-81af-d3284c073c1a','gissard0@harvard.edu','$2y$10$42E.edBFNa5DnF1Jo4PFAekXP/awVNYDwE5.h219m2I7Ud8QrJjkq'),('58f9678e-c742-11ed-81af-d3284c073c1a','ncrumb1@wisc.edu','$2y$10$30Lu8NBepzLRwBAbYDX20edv1zuXMgeDrDLcEXxLIqaNDbu.Tf8Iu'),('58f97ac6-c742-11ed-81af-d3284c073c1a','cromanin2@nature.com','$2y$10$bY.iyLaTtuMQTZOxkL9tEuLX8UCiUssQpSZXYLafEXUwqXgoWYzyO'),('58f98566-c742-11ed-81af-d3284c073c1a','fcumberpatch3@themeforest.net','$2y$10$dqkdCUuDxsdoMFOA3VoPKO6J8ekGeUFMo3r3/49mA0rO3jDGFWIsW');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -318,4 +289,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-03-12 12:40:09
+-- Dump completed on 2023-03-20 18:21:24
--- theater.sql
+++ theater.sql
@@ -3,136 +3,118 @@ CREATE DATABASE IF NOT EXISTS theater CHARACTER SET utf8mb4 COLLATE utf8mb4_gene
 USE theater;
 
 CREATE TABLE users (
-    id_users INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    id_users CHAR(36) PRIMARY KEY NOT NULL,
     email VARCHAR(50) NOT NULL,
     users_password VARCHAR(60) NOT NULL
 ) ENGINE = INNODB;
 
 CREATE TABLE complexes (
-    id_complexes INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    id_complexes CHAR(36) PRIMARY KEY NOT NULL,
     name VARCHAR(50) NOT NULL,
     address VARCHAR(50) NOT NULL
 ) ENGINE = INNODB;
 
 CREATE TABLE admins (
-    id_admins INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    id_admins CHAR(36) PRIMARY KEY NOT NULL,
     email VARCHAR(50) NOT NULL,
     admins_password VARCHAR(60) NOT NULL  
 ) ENGINE = INNODB;
 
 CREATE TABLE movie (
-    id_movie INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    id_movie CHAR(36) PRIMARY KEY NOT NULL,
     title VARCHAR(50) NOT NULL,
     synopsis TEXT NOT NULL
 ) ENGINE = INNODB;
 
 CREATE TABLE rate (
-    id_rate INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    id_rate CHAR(36) PRIMARY KEY NOT NULL,
     category VARCHAR(50) NOT NULL,
     price FLOAT NOT NULL
 ) ENGINE = INNODB;
 
 CREATE TABLE movie_theater (
-    id_movie_theater INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    id_movie_theater CHAR(36) PRIMARY KEY NOT NULL,
     number_of_the_movie_room INT NOT NULL,
     number_of_seat INT NOT NULL,
-    id_movie INT NOT NULL,
+    id_movie CHAR(36) NOT NULL,
     FOREIGN KEY (id_movie) REFERENCES movie (id_movie)
 ) ENGINE = INNODB;
 
 CREATE TABLE reservation (
-    id_reservation INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    id_reservation CHAR(36) PRIMARY KEY NOT NULL,
     number_of_seat INT NOT NULL,
     name VARCHAR(50) NOT NULL,
     email VARCHAR(50) NOT NULL
 ) ENGINE = INNODB;
 
 CREATE TABLE admins_complexes (
-    id_admins INT NOT NULL,
+    id_admins CHAR(36) NOT NULL,
     FOREIGN KEY (id_admins) REFERENCES admins (id_admins),
-    id_complexes INT NOT NULL,
+    id_complexes CHAR(36) NOT NULL,
     FOREIGN KEY (id_complexes) REFERENCES complexes (id_complexes)
 ) ENGINE = INNODB;
 
 CREATE TABLE session (
-    id_session INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    id_session CHAR(36) PRIMARY KEY NOT NULL,
     day DATE NOT NULL,
     schedule TIME NOT NULL,
-    id_users INT NOT NULL,
+    id_users CHAR(36) NOT NULL,
     FOREIGN KEY (id_users) REFERENCES users (id_users),
-    id_complexes INT NOT NULL,
+    id_complexes CHAR(36) NOT NULL,
     FOREIGN KEY (id_complexes) REFERENCES complexes (id_complexes),
-    id_rate INT NOT NULL,
-    FOREIGN KEY (id_rate) REFERENCES rate (id_rate)
-) ENGINE = INNODB;
-
-CREATE TABLE session_reservation (
-    id_session INT NOT NULL,
-    FOREIGN KEY (id_session) REFERENCES session (id_session),
-    id_reservation INT NOT NULL,
+    id_rate CHAR(36) NOT NULL,
+    FOREIGN KEY (id_rate) REFERENCES rate (id_rate),
+    id_reservation CHAR(36) NOT NULL,
     FOREIGN KEY (id_reservation) REFERENCES reservation (id_reservation)
 ) ENGINE = INNODB;
 
 CREATE TABLE session_movie_theater (
-    id_session INT NOT NULL,
+    id_session CHAR(36) NOT NULL,
     FOREIGN KEY (id_session) REFERENCES session (id_session),
-    id_movie_theater INT NOT NULL,
+    id_movie_theater CHAR(36) NOT NULL,
     FOREIGN KEY (id_movie_theater) REFERENCES movie_theater (id_movie_theater)
 ) ENGINE = INNODB;
 
-INSERT INTO rate (category, price) VALUES ('Plein Tarif', 9.20);
-INSERT INTO rate (category, price) VALUES ('Étudiant', 7.60);
-INSERT INTO rate (category, price) VALUES ('Moins de 14 ans', 5.90);
+INSERT INTO rate (id_rate, category, price) VALUES (UUID(), 'Plein Tarif', 9.20);
+INSERT INTO rate (id_rate, category, price) VALUES (UUID(), 'Étudiant', 7.60);
+INSERT INTO rate (id_rate, category, price) VALUES (UUID(), 'Moins de 14 ans', 5.90);
 
-INSERT INTO users (email, users_password) VALUES ('gissard0@harvard.edu','$2y$10$42E.edBFNa5DnF1Jo4PFAekXP/awVNYDwE5.h219m2I7Ud8QrJjkq');
-INSERT INTO users (email, users_password) VALUES ('ncrumb1@wisc.edu','$2y$10$30Lu8NBepzLRwBAbYDX20edv1zuXMgeDrDLcEXxLIqaNDbu.Tf8Iu'); 
-INSERT INTO users (email, users_password) VALUES ('cromanin2@nature.com','$2y$10$bY.iyLaTtuMQTZOxkL9tEuLX8UCiUssQpSZXYLafEXUwqXgoWYzyO');
-INSERT INTO users (email, users_password) VALUES ('fcumberpatch3@themeforest.net','$2y$10$dqkdCUuDxsdoMFOA3VoPKO6J8ekGeUFMo3r3/49mA0rO3jDGFWIsW');
+INSERT INTO users (id_users, email, users_password) VALUES (UUID(), 'gissard0@harvard.edu','$2y$10$42E.edBFNa5DnF1Jo4PFAekXP/awVNYDwE5.h219m2I7Ud8QrJjkq');
+INSERT INTO users (id_users, email, users_password) VALUES (UUID(), 'ncrumb1@wisc.edu','$2y$10$30Lu8NBepzLRwBAbYDX20edv1zuXMgeDrDLcEXxLIqaNDbu.Tf8Iu'); 
+INSERT INTO users (id_users, email, users_password) VALUES (UUID(), 'cromanin2@nature.com','$2y$10$bY.iyLaTtuMQTZOxkL9tEuLX8UCiUssQpSZXYLafEXUwqXgoWYzyO');
+INSERT INTO users (id_users, email, users_password) VALUES (UUID(), 'fcumberpatch3@themeforest.net','$2y$10$dqkdCUuDxsdoMFOA3VoPKO6J8ekGeUFMo3r3/49mA0rO3jDGFWIsW');
 
-INSERT INTO complexes (name, address) VALUES ('Thoughtbridge','24593 Tomscot Road');
-INSERT INTO complexes (name, address) VALUES ('Pixope', '801 Scofield Way');
-INSERT INTO complexes (name, address) VALUES ('Dazzlesphere','27792 Maywood Road');
-INSERT INTO complexes (name, address) VALUES ('Quire', '68252 Esker Plaza');
+INSERT INTO complexes (id_complexes, name, address) VALUES (UUID(), 'Thoughtbridge','24593 Tomscot Road');
+INSERT INTO complexes (id_complexes, name, address) VALUES (UUID(), 'Pixope', '801 Scofield Way');
+INSERT INTO complexes (id_complexes, name, address) VALUES (UUID(), 'Dazzlesphere','27792 Maywood Road');
+INSERT INTO complexes (id_complexes, name, address) VALUES (UUID(), 'Quire', '68252 Esker Plaza');
 
-INSERT INTO admins (email, admins_password) VALUES ('choyer0@facebook.com', '$2y$10$Uh38.Z6VwzcoATDQvLBrBevYrEXX3aRz/Ta9vAaS/lnMyPj0Et3ri');
-INSERT INTO admins (email, admins_password) VALUES ('spenhall1@netlog.com', '$2y$10$cCXnEYzk4O1K/iK896fCO.lsxS18.Qc2GlYsZK1W68mvpjQXOLp/u');
-INSERT INTO admins (email, admins_password) VALUES ('fscruton2@about.com', '$2y$10$WldmBh0chrIbs7yMItwNneW82jogNd0ULCTKN3k.W5c.nR8JjSKBW');
-INSERT INTO admins (email, admins_password) VALUES ('kpapierz3@blogs.com', '$2y$10$I/FTiBhGwgAtGKLN2wz7sOIOX.lgVi3w3wS.lLNmIZPVE/tvszR1K');
+INSERT INTO admins (id_admins, email, admins_password) VALUES (UUID(), 'choyer0@facebook.com', '$2y$10$Uh38.Z6VwzcoATDQvLBrBevYrEXX3aRz/Ta9vAaS/lnMyPj0Et3ri');
+INSERT INTO admins (id_admins, email, admins_password) VALUES (UUID(), 'spenhall1@netlog.com', '$2y$10$cCXnEYzk4O1K/iK896fCO.lsxS18.Qc2GlYsZK1W68mvpjQXOLp/u');
+INSERT INTO admins (id_admins, email, admins_password) VALUES (UUID(), 'fscruton2@about.com', '$2y$10$WldmBh0chrIbs7yMItwNneW82jogNd0ULCTKN3k.W5c.nR8JjSKBW');
+INSERT INTO admins (id_admins, email, admins_password) VALUES (UUID(), 'kpapierz3@blogs.com', '$2y$10$I/FTiBhGwgAtGKLN2wz7sOIOX.lgVi3w3wS.lLNmIZPVE/tvszR1K');
 
-INSERT INTO movie (title, synopsis) VALUES ('Good Lawyer''s Wife, A (Baramnan gajok)', 'Cysto & recto w grf/pros');
-INSERT INTO movie (title, synopsis) VALUES ('Tadpole', 'Bact smear-eye');
-INSERT INTO movie (title, synopsis) VALUES ('Pericles on 31st Street', 'Parathyroid biopsy');
-INSERT INTO movie (title, synopsis) VALUES ('Great Race, The', 'Wound catheter irrigat');
-INSERT INTO movie (title, synopsis) VALUES ('Kedma', 'Close reduc-femur epiphy');
+INSERT INTO movie (id_movie, title, synopsis) VALUES (UUID(), 'Good Lawyer''s Wife, A (Baramnan gajok)', 'Cysto & recto w grf/pros');
+INSERT INTO movie (id_movie, title, synopsis) VALUES (UUID(), 'Tadpole', 'Bact smear-eye');
+INSERT INTO movie (id_movie, title, synopsis) VALUES (UUID(), 'Pericles on 31st Street', 'Parathyroid biopsy');
+INSERT INTO movie (id_movie, title, synopsis) VALUES (UUID(), 'Great Race, The', 'Wound catheter irrigat');
+INSERT INTO movie (id_movie, title, synopsis) VALUES (UUID(), 'Kedma', 'Close reduc-femur epiphy');
 
-INSERT INTO movie_theater (number_of_the_movie_room, number_of_seat, id_movie) VALUES (8, 100,1);
-INSERT INTO movie_theater (number_of_the_movie_room, number_of_seat, id_movie) VALUES (7, 200,2);
-INSERT INTO movie_theater (number_of_the_movie_room, number_of_seat, id_movie) VALUES (9, 230,3);
-INSERT INTO movie_theater (number_of_the_movie_room, number_of_seat, id_movie) VALUES (5, 140,4);
-INSERT INTO movie_theater (number_of_the_movie_room, number_of_seat, id_movie) VALUES (3, 250,5);
+INSERT INTO movie_theater (id_movie_theater, number_of_the_movie_room, number_of_seat) VALUES (UUID(), 8, 100);
+INSERT INTO movie_theater (id_movie_theater, number_of_the_movie_room, number_of_seat) VALUES (UUID(), 7, 200);
+INSERT INTO movie_theater (id_movie_theater, number_of_the_movie_room, number_of_seat) VALUES (UUID(), 9, 230);
+INSERT INTO movie_theater (id_movie_theater, number_of_the_movie_room, number_of_seat) VALUES (UUID(), 5, 140);
+INSERT INTO movie_theater (id_movie_theater, number_of_the_movie_room, number_of_seat) VALUES (UUID(), 3, 250);
 
-INSERT INTO reservation (number_of_seat, name, email) VALUES (1, 'Josiah Torn', 'jtorn0@youtu.be');
-INSERT INTO reservation (number_of_seat, name, email) VALUES (2, 'Adrian Leishman', 'aleishman1@hud.gov');
-INSERT INTO reservation (number_of_seat, name, email) VALUES (3, 'Lyle Beak', 'lbeak2@comcast.net');
-INSERT INTO reservation (number_of_seat, name, email) VALUES (4, 'Kiley Cubbin', 'kcubbin3@blog.com');
-INSERT INTO reservation (number_of_seat, name, email) VALUES (5, 'Thorndike Hollidge', 'thollidge4@a8.net');
+INSERT INTO reservation (id_reservation, number_of_seat, name, email) VALUES (UUID(), 1, 'Josiah Torn', 'jtorn0@youtu.be');
+INSERT INTO reservation (id_reservation, number_of_seat, name, email) VALUES (UUID(), 2, 'Adrian Leishman', 'aleishman1@hud.gov');
+INSERT INTO reservation (id_reservation, number_of_seat, name, email) VALUES (UUID(), 3, 'Lyle Beak', 'lbeak2@comcast.net');
+INSERT INTO reservation (id_reservation, number_of_seat, name, email) VALUES (UUID(), 4, 'Kiley Cubbin', 'kcubbin3@blog.com');
+INSERT INTO reservation (id_reservation, number_of_seat, name, email) VALUES (UUID(), 5, 'Thorndike Hollidge', 'thollidge4@a8.net');
 
-INSERT INTO session (day, schedule, id_users, id_complexes, id_rate) VALUES ('2023-02-27', '9:15:00',1,1,1);
-INSERT INTO session (day, schedule, id_users, id_complexes, id_rate) VALUES ('2023-02-23', '9:20:00',2,2,2);
-INSERT INTO session (day, schedule, id_users, id_complexes, id_rate) VALUES ('2023-02-24', '9:25:00',3,3,3);
-
-INSERT INTO admins_complexes (id_admins, id_complexes) VALUES (1,1);
-INSERT INTO admins_complexes (id_admins, id_complexes) VALUES (2,2);
-INSERT INTO admins_complexes (id_admins, id_complexes) VALUES (3,3);
-INSERT INTO admins_complexes (id_admins, id_complexes) VALUES (4,4);
-
-INSERT INTO session_movie_theater (id_session, id_movie_theater) VALUES (1,1);
-INSERT INTO session_movie_theater (id_session, id_movie_theater) VALUES (2,2);
-INSERT INTO session_movie_theater (id_session, id_movie_theater) VALUES (3,3);
-
-INSERT INTO session_reservation (id_session, id_reservation) VALUES (1,1);
-INSERT INTO session_reservation (id_session, id_reservation) VALUES (2,2);
-INSERT INTO session_reservation (id_session, id_reservation) VALUES (3,3);
+INSERT INTO session (id_session, day, schedule) VALUES (UUID(), '2023-02-27', '9:15:00');
+INSERT INTO session (id_session, day, schedule) VALUES (UUID(), '2023-02-23', '9:20:00');
+INSERT INTO session (id_session, day, schedule) VALUES (UUID(), '2023-02-24', '9:25:00');
 
 GRANT ALL PRIVILEGES ON theater.* TO 'choyer'@'localhost' WITH GRANT OPTION;
 GRANT ALL PRIVILEGES ON theater.* TO 'spenhall'@'localhost' WITH GRANT OPTION;
